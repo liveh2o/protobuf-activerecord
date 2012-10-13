9e7b416519c16904be746f34fdde94d055f59ea3
Adam Hutchison <adam@moneydesktop.com>
2012-10-12 19:53:04 -0600

    Added some errors to make exceptions clearer.

--- lib/protoable/errors.rb
+++ lib/protoable/errors.rb
@@ -1,4 +1,21 @@
 module Protoable
+
+  # = Protoable errors
+  #
+  # Generic Protoable exception class
+  class ProtoableError < StandardError
+  end
+
+  # Raised by Protoable.protobuf_column_convert when the convert method
+  # given is not defined, nil, or not callable.
+  class ColumnConverterError < ProtoableError
+  end
+
+  # Raised by Protoable.protobuf_column_transform when the transformer method
+  # given is not defined, nil, or not callable.
+  class ColumnTransformerError < ProtoableError
+  end
+
   module Errors
     OK = 200
     BAD_REQUEST = 400
