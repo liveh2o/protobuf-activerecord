28c64307f2ae83a9af250c0c805baf13cc335e16
Adam Hutchison <adam@moneydesktop.com>
2012-10-14 14:59:16 -0600

    Don't need the base-model helpers any more.

--- lib/protoable.rb
+++ lib/protoable.rb
@@ -6,21 +6,10 @@ require 'protoable/serialization'
 
 module Protoable
   def self.included(klass)
-    klass.extend Protoable::ClassMethods
     klass.extend Protoable::Fields
 
     klass.__send__(:include, Protoable::Convert)
     klass.__send__(:include, Protoable::Persistence)
     klass.__send__(:include, Protoable::Serialization)
-  end
-
-  module ClassMethods
-    def _protobuf_base_model
-      self
-    end
-  end
-
-  def _protobuf_base_model
-    self.class._protobuf_base_model
   end
 end
