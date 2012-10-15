03b4981b7aa773c0509572415afadff1db145e67
Adam Hutchison <adam@moneydesktop.com>
2012-10-14 20:34:25 -0600

    To ensure we defer to user-specified column and field converters, we should check for them first when running through the case statement looking for converters.
    When converting to time/date/datetime, we now return an object whose class matches the database column. When converter from time/date/datetime, we really just need to call #to_i – there's no need for a method. Also, don't attempt to gracefully handle conversions – let's leave that up to the user.

--- lib/protoable/convert.rb
+++ lib/protoable/convert.rb
@@ -5,34 +5,30 @@ module Protoable
     end
 
     module ClassMethods
-      def _convert_int64_to_datetime(protobuf_value)
-        return protobuf_value.respond_to?(:to_i) ? Time.at(protobuf_value.to_i) : protobuf_value
+      def convert_int64_to_time(int64)
+        Time.at(int64.to_i)
       end
 
-      def _convert_datetime_to_int64(value)
-        return value.respond_to?(:to_i) ? value.to_i : value
+      def convert_int64_to_date(int64)
+        convert_int64_to_time(int64).to_date
       end
 
-      def _protobuf_date_column?(key)
-        _protobuf_column_types[:date] && _protobuf_column_types[:date].include?(key)
+      def convert_int64_to_datetime(int64)
+        convert_int64_to_time(int64).to_datetime
       end
 
-      def _protobuf_datetime_column?(key)
-        _protobuf_column_types[:datetime] && _protobuf_column_types[:datetime].include?(key)
-      end
-      
       def _protobuf_convert_columns(key, value)
         value = case
-                when _protobuf_datetime_column?(key) then
-                  _convert_datetime_to_int64(value)
-                when _protobuf_timestamp_column?(key) then
-                  _convert_datetime_to_int64(value)
-                when _protobuf_time_column?(key) then
-                  _convert_datetime_to_int64(value)
-                when _protobuf_date_column?(key) then
-                  _convert_datetime_to_int64(value)
                 when _protobuf_field_converters.has_key?(key.to_sym) then
                   _protobuf_field_converters[key.to_sym].call(value)
+                when _protobuf_datetime_column?(key) then
+                  value.to_i
+                when _protobuf_timestamp_column?(key) then
+                  value.to_i
+                when _protobuf_time_column?(key) then
+                  value.to_i
+                when _protobuf_date_column?(key) then
+                  value.to_i
                 else
                   value
                 end
@@ -42,16 +38,16 @@ module Protoable
 
       def _protobuf_convert_fields(key, value)
         value = case
-                when _protobuf_datetime_column?(key) then
-                  _convert_int64_to_datetime(value)
-                when _protobuf_timestamp_column?(key) then
-                  _convert_int64_to_datetime(value)
-                when _protobuf_time_column?(key) then
-                  _convert_int64_to_datetime(value)
-                when _protobuf_date_column?(key) then
-                  _convert_int64_to_datetime(value)
                 when _protobuf_column_converters.has_key?(key.to_sym) then
                   _protobuf_column_converters[key.to_sym].call(value)
+                when _protobuf_date_column?(key) then
+                  convert_int64_to_date(value)
+                when _protobuf_datetime_column?(key) then
+                  convert_int64_to_datetime(value)
+                when _protobuf_time_column?(key) then
+                  convert_int64_to_time(value)
+                when _protobuf_timestamp_column?(key) then
+                  convert_int64_to_time(value)
                 else
                   value
                 end
@@ -59,12 +55,20 @@ module Protoable
         return value
       end
 
-      def _protobuf_timestamp_column?(key)
-        _protobuf_column_types[:timestamp] && _protobuf_column_types[:timestamp].include?(key)
+      def _protobuf_date_column?(key)
+        _protobuf_column_types[:date] && _protobuf_column_types[:date].include?(key)
+      end
+
+      def _protobuf_datetime_column?(key)
+        _protobuf_column_types[:datetime] && _protobuf_column_types[:datetime].include?(key)
       end
 
       def _protobuf_time_column?(key)
         _protobuf_column_types[:time] && _protobuf_column_types[:time].include?(key)
+      end
+
+      def _protobuf_timestamp_column?(key)
+        _protobuf_column_types[:timestamp] && _protobuf_column_types[:timestamp].include?(key)
       end
     end
   end
