f6ffc6594288a0f913724ece22c0906a606f8027
Adam Hutchison <adam@moneydesktop.com>
2012-10-25 13:32:45 -0600

    Need to call fetch when checking if a column type exists and convert dates to time before converting them to integers (Date doesn't respond to to_i).

--- lib/protoable/convert.rb
+++ lib/protoable/convert.rb
@@ -22,7 +22,7 @@ module Protoable
                 when _protobuf_column_converters.has_key?(key.to_sym) then
                   _protobuf_column_converters[key.to_sym].call(value)
                 when _protobuf_date_column?(key) then
-                  value.to_i
+                  value.to_time.to_i
                 when _protobuf_datetime_column?(key) then
                   value.to_i
                 when _protobuf_time_column?(key) then
@@ -56,19 +56,19 @@ module Protoable
       end
 
       def _protobuf_date_column?(key)
-        _protobuf_column_types[:date] && _protobuf_column_types[:date].include?(key)
+        _protobuf_column_types.fetch(:date, false) && _protobuf_column_types[:date].include?(key)
       end
 
       def _protobuf_datetime_column?(key)
-        _protobuf_column_types[:datetime] && _protobuf_column_types[:datetime].include?(key)
+        _protobuf_column_types.fetch(:datetime, false) && _protobuf_column_types[:datetime].include?(key)
       end
 
       def _protobuf_time_column?(key)
-        _protobuf_column_types[:time] && _protobuf_column_types[:time].include?(key)
+        _protobuf_column_types.fetch(:time, false) && _protobuf_column_types[:time].include?(key)
       end
 
       def _protobuf_timestamp_column?(key)
-        _protobuf_column_types[:timestamp] && _protobuf_column_types[:timestamp].include?(key)
+        _protobuf_column_types.fetch(:timestamp, false) && _protobuf_column_types[:timestamp].include?(key)
       end
     end
   end
