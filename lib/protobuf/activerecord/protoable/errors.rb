0c4d0445deec4bffdee908c2a701c2181d82f49c
Adam Hutchison <adam@moneydesktop.com>
2013-02-17 20:14:31 -0700

    Removing unused errors.

--- lib/protobuf/activerecord/protoable/errors.rb
+++ lib/protobuf/activerecord/protoable/errors.rb
@@ -6,21 +6,11 @@ module Protoable
   class ProtoableError < StandardError
   end
 
-  # Raised by Protoable.protoable_attribute when the convert method given is
-  # not callable.
-  class AttributeConverterError < ProtoableError
-  end
-
   # Raised by Protoable.attribute_from_proto when the transformer method
   # given is not callable.
   class AttributeTransformerError < ProtoableError
   end
 
-  # Raised by Protoable.convert_field when the convert method
-  # given not callable.
-  class FieldConverterError < ProtoableError
-  end
-  
   # Raised by Protoable.field_from_record when the convert method
   # given not callable.
   class FieldTransformerError < ProtoableError
