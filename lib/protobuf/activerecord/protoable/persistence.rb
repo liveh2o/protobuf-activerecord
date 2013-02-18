5c74cfd48e11aa666f1c09323ba38ff0eca98b0f
Adam Hutchison <adam@moneydesktop.com>
2013-02-17 20:11:57 -0700

    Removing all of the xxx_from_proto methods.

--- lib/protobuf/activerecord/protoable/persistence.rb
+++ lib/protobuf/activerecord/protoable/persistence.rb
@@ -30,17 +30,6 @@ module Protoable
 
         super(attributes, options)
       end
-
-      # Creates an object from the given protobuf message, if it's valid. The
-      # newly created object is returned if it was successfully saved or not.
-      #
-      def create_from_proto(proto, options = {})
-        attributes = attributes_from_proto(proto)
-
-        yield(attributes) if block_given?
-
-        self.create(attributes, options)
-      end
     end
 
     # :nodoc:
@@ -48,13 +37,6 @@ module Protoable
       attributes = attributes_from_proto(proto) if attributes.is_a?(::Protobuf::Message)
 
       super(attributes, options)
-    end
-
-    # Destroys the record. Mainly wrapped to provide a consistent API and
-    # a convient way to override protobuf-specific destroy behavior.
-    #
-    def destroy_from_proto
-      destroy
     end
 
     # :nodoc:
@@ -69,17 +51,6 @@ module Protoable
       attributes = attributes_from_proto(attributes) if attributes.is_a?(::Protobuf::Message)
 
       super(attributes, options)
-    end
-
-    # Update a record from a proto message. Accepts an optional block.
-    # If block is given, yields the attributes that would be updated.
-    #
-    def update_from_proto(proto, options = {})
-      attributes = attributes_from_proto(proto)
-
-      yield(attributes) if block_given?
-
-      update_attributes(attributes, options)
     end
   end
 end
--- spec/protoable/persistence_spec.rb
+++ spec/protoable/persistence_spec.rb
@@ -30,58 +30,7 @@ describe Protoable::Persistence do
     end
   end
 
-  describe ".create_from_proto" do
-    it "initializes a new object with attributes from the given protobuf message" do
-      user = User.create_from_proto(proto)
-      attributes = user.attributes.slice("first_name", "last_name", "email")
-      attributes.should eq user_attributes.stringify_keys
-    end
-
-    context "when a block is given" do
-      it "yields to the block with attributes from the given protobuf message" do
-        yielded = nil
-
-        User.create_from_proto(proto) do |attributes|
-          yielded = "boom!"
-        end
-
-        yielded.should eq "boom!"
-      end
-    end
-
-    context "when the object is valid" do
-      it "saves the record" do
-        user = User.create_from_proto(proto)
-        user.persisted?.should be_true
-      end
-
-      context "when an error occurs while saving" do
-        before { User.any_instance.stub(:create).and_raise(RuntimeError) }
-
-        it "raises an exception" do
-          expect { User.create_from_proto(proto) }.to raise_exception
-        end
-      end
-    end
-
-    context "when the object is not valid" do
-      before { User.any_instance.stub(:valid?).and_return(false) }
-
-      it "returns the new object, unsaved" do
-        user = User.create_from_proto(proto)
-        user.persisted?.should be_false
-      end
-    end
-  end
-
-  describe "#destroy_from_proto" do
-    it "destroys the object" do
-      user.should_receive(:destroy)
-      user.destroy_from_proto
-    end
-  end
-
-  describe ".update_attributes" do
+  describe "#update_attributes" do
     it "accepts a protobuf message" do
       User.any_instance.should_receive(:save)
       user.update_attributes(proto)
@@ -93,7 +42,7 @@ describe Protoable::Persistence do
     end
   end
 
-  describe ".update_attributes!" do
+  describe "#update_attributes!" do
     it "accepts a protobuf message" do
       User.any_instance.should_receive(:save!)
       user.update_attributes!(proto)
@@ -102,52 +51,6 @@ describe Protoable::Persistence do
     it "accepts a hash" do
       User.any_instance.should_receive(:save!)
       user.update_attributes!(user_attributes)
-    end
-  end
-
-  describe "#update_from_proto" do
-    it "updates the object with attributes from the given protobuf message" do
-      user.should_receive(:assign_attributes).with(user_attributes, {})
-      user.update_from_proto(proto)
-    end
-
-    context "when a block is given" do
-      it "yields to the block with attributes from the given protobuf message" do
-        yielded = nil
-
-        user.update_from_proto(proto) do |attributes|
-          yielded = "boom!"
-        end
-
-        yielded.should eq "boom!"
-      end
-    end
-
-    context "when the object is valid" do
-      before { user.stub(:valid?).and_return(true) }
-
-      context "when saving is successful" do
-        it "returns true" do
-          user.stub(:assign_attributes).and_return(true)
-          user.update_from_proto(proto).should be_true
-        end
-      end
-
-      context "when an error occurs while saving" do
-        before { user.stub(:assign_attributes).and_raise(RuntimeError) }
-
-        it "raises an exception" do
-          expect { user.update_from_proto }.to raise_exception
-        end
-      end
-    end
-
-    context "when the object is invalid" do
-      before { user.stub(:valid?).and_return(false) }
-
-      it "returns false" do
-        user.update_from_proto(proto).should be_false
-      end
     end
   end
 end
