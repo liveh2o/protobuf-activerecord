24cdbd81e4e979974780ca30cfaa94e7cd2104a8
Adam Hutchison <adam@moneydesktop.com>
2013-07-19 15:03:39 -0600

    Ensure dependencies are loaded.

--- lib/protobuf-activerecord.rb
+++ lib/protobuf-activerecord.rb
@@ -1,5 +1,8 @@
+require 'active_record'
+require 'active_support/concern'
 require 'heredity'
 require 'protobuf'
+
 require 'protobuf/activerecord/version'
 require 'protobuf/activerecord/protobuf_ext/message'
 require 'protobuf/activerecord/protoable'
