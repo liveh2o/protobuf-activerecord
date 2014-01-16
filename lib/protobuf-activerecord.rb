aafba918ba6223df7100b06c6c5315ec448922de
Adam Hutchison <liveh2o@gmail.com>
2014-01-15 21:04:28 -0700

    Attempt to load protected_attributes
    To support Rails 4 apps that use Mass-Assignment Security, attempt to
    load the protected_attributes gem. If it's not present, move along...

--- lib/protobuf-activerecord.rb
+++ lib/protobuf-activerecord.rb
@@ -3,6 +3,14 @@ require 'active_support/concern'
 require 'heredity'
 require 'protobuf'
 
+# To support Rails 4 apps that use Mass-Assignment Security, attempt to
+# load the protected_attributes gem. If it's not present, move along...
+begin
+  require 'protected_attributes'
+rescue LoadError
+  # These aren't the droids you're looking for... move along.
+end
+
 require 'protobuf/active_record/config'
 require 'protobuf/active_record/model'
 require 'protobuf/active_record/version'
