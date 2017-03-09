6116f459d7065035e4f40137b52cb42e9de30688
Brandon Dewitt <brandonsdewitt@gmail.com>
2017-03-09 15:39:42 -0700

    add concurrent ruby and make connection mgmt async with new middleware

---
+++ lib/protobuf/active_record/middleware/connection_management_async.rb
@@ -0,0 +1,50 @@
+require "concurrent"
+require "thread"
+
+module Protobuf
+  module ActiveRecord
+    module Middleware
+      class ConnectionManagementAsync
+        START_MUTEX = ::Mutex.new
+
+        def self.start_timed_task!
+          if timed_task_started.false?
+            START_MUTEX.synchronize do
+              return if timed_task_started.true?
+
+              timed_task = ::Concurrent::TimerTask.new(:execution_interval => 6, :timeout_interval => 5) do
+                ::ActiveRecord::Base.clear_active_connections!
+              end
+
+              timed_task.execute
+              timed_task_started.make_true
+            end
+          end
+        end
+
+        def self.timed_task_started
+          if @timed_task_started.nil?
+            @timed_task_started = ::Concurrent::AtomicBoolean.new(false)
+          end
+
+          @timed_task_started
+        end
+
+        def initialize(app)
+          @app = app
+        end
+
+        def call(env)
+          def call(env)
+            @app.call(env)
+          end
+
+          self.class.start_timed_task!
+          call(env)
+        end
+
+        timed_task_started
+      end
+    end
+  end
+end
